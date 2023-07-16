b9cc44b45535a00ba05d0e5cde3dc9271e759f2e
han <ips3471@gmail.com>
2023-07-16 23:59:48 +0900

    next 13.4.1

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		appDir: true,
-	},
-	output: 'export',
 	reactStrictMode: true,
+	output: 'export',
 };
 
 module.exports = nextConfig;
